67ede176766a9bd5fdfd0a6ba99d05a21fb9e05e
Vinicius Cajui <viniciuscajui22@gmail.com>
2022-09-09 21:44:38 -0300

    apostila05: bloco de codigo 2.7.1 encerrado

--- src/index.js
+++ src/index.js
@@ -35,6 +35,14 @@ class App extends React.Component {
         }
         return sul ? 'Outuno' : 'Primavera'
     }
+
+    icones = {
+        "Primavera" : "fa-seedling",
+        "Verão": "fa-umbrella-beach",
+        "Outono": "fa-tree",
+        "Inverno": "fa-snowman"
+    }
+
     render() {
         return (
             <div>
